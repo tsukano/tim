31e13178e331e40b04cad4054e9403b994d8da3e
Satoshi.Utsumi <satoshi.utsumi@inte.co.jp>
2011-08-03 16:45:09 +0900

    zabbixapi対応

--- mail_picker/mail_picker.rb
+++ mail_picker/mail_picker.rb
@@ -3,12 +3,7 @@
 require 'net/pop'
 require 'rubygems'
 #require 'trac4r'
-require 'redmine_client'
-require 'zabbixapi'
-require 'json'
 
-require 'action_mailer' 
-require 'tmail'
 require 'nkf'
 require 'yaml'
 require 'rexchange'
@@ -67,13 +62,13 @@ module MailPicker
 
     return if conf.empty?
 
-    RedmineClient::Base.configure do
-      self.site = conf[:redmine_url]
-      self.user = conf[:redmine_user_id]
-      self.password = conf[:redmine_user_password]
-    end
+#    RedmineClient::Base.configure do
+#      self.site = conf[:redmine_url]
+#      self.user = conf[:redmine_user_id]
+#      self.password = conf[:redmine_user_password]
+#    end
     mapping_fields = ConfUtil.get_mapping_field_list(conf.keys)
-    p 'test'
+
 #    MUST_WRITE_CONF.each do |conf_field|
 #      if conf[conf_field] == nil || conf[conf_field].blank?
 #        $hinemosTracLog.puts_message "Caution. You must write configuration about #{conf_field}."
@@ -85,25 +80,61 @@ module MailPicker
 #    return if mail_duplicate_checker.message_id_list == nil # not found the file
 
 	  begin
-	    p 'test2'
-	    get_event_from_zabbix(conf)
+	    
+	    if conf[:information_get_mode] == 0
+	      p 'mail'
+        require 'action_mailer' 
+        require 'tmail'
+	      mail_session = MailSession.new(conf)
+	      p mail_session.tmail_list
+	      mail_session.tmail_list.each_with_index do |t_mail, i|
+	        p 't1'
+          next if t_mail.subject.index(conf[:target_mail_title]) != 0
+          mail_subject = MAIL_ENCODER.call(t_mail.subject.to_s)
+          mail_body = t_mail.body.to_s
+        
+          regist_issue(mail_subject, mail_body, conf, mapping_fields)
+        end
+        mail_session.finalize
+	    else
+	      p 'zabbixapi'
+	      require 'zabbixapi'
+	      zbx = Zabbix::ZabbixApi.new(conf[:zabbix_url], conf[:zabbix_user_id], conf[:zabbix_user_password])
+	      h = zbx.get_host_id('s-ibs-portal-stg01')
+	      
+	      message_get_alert = { 
+          :method => 'alert.get', 
+          :params => { 
+             :output => 'extend', 
+          }, 
+          :auth => zbx.auth 
+        } 
+        alerts = zbx.do_request(message_get_alert)
 
-	    return
-#	  	mail_session = MailSession.new(conf)ggggggggggggggggggsqruby
-	  rescue => e
-	    p e
-	    p e.message
+        alerts.each_with_index do |alert, i|
+          
+          mail_subject = alert["subject"].to_s
+          next if mail_subject.index(conf[:target_mail_title]) != 0
+          
+          mail_body = alert["message"]
+          regist_issue(mail_subject, mail_body, conf, mapping_fields)
+        end
+
+	    end
 #	    $hinemosTracLog.puts_message "Failure to access the mail server. Please check mail server configuration. "
 	    return
 	  else
 #	    $hinemosTracLog.puts_message "Success to access the mail server."
 	  end
+	end
 
     # Issue model on the client side
-    mail_session.tmail_list.each_with_index do |t_mail, i|
+#################################################################################################################
+#    mail_session.tmail_list.each_with_index do |t_mail, i|
 
-  	  next if t_mail.subject.index(conf[:target_mail_title]) != 0
-        mail_body = t_mail.body.to_s
+#  	  next if t_mail.subject.index(conf[:target_mail_title]) != 0
+
+#      mail_body = t_mail.body.to_s
 
 #      if MailPicker.target_mail?(t_mail, conf)
 #        next if mail_duplicate_checker.has_created_ticket?(t_mail.message_id)
@@ -113,84 +144,84 @@ module MailPicker
 #        trac = Trac.new(conf[:trac_url] + TRAC_URL_SUFFIX,
 #        								conf[:trac_user_id], 
 #        								conf[:trac_user_password])
-
-        custom_fields = Hash.new
+    def regist_issue(mail_subject, mail_body, conf, mapping_fields)
+      require 'redmine_client'
+      RedmineClient::Base.configure do
+        self.site = conf[:redmine_url]
+        self.user = conf[:redmine_user_id]
+        self.password = conf[:redmine_user_password]
+      end
+      custom_fields = Hash.new
         
 #        mail_parser = MailParser.new( t_mail.body.to_s,
 #        															t_mail.date.to_s)
 
-
-        mail_body.each_line do |line| 
-
-          mapping_fields.each do |mapping_field|
-            if line.index(conf[(CONF_MAPPING_HEADER + mapping_field).to_sym]) == 0
-              parse_mapping_value =  /#{conf[(CONF_MAPPING_HEADER + mapping_field).to_sym]} = /.match(line)
+      mail_body.each_line do |line|
+        mapping_fields.each do |mapping_field|
+          if line.index(conf[(CONF_MAPPING_HEADER + mapping_field).to_sym]) == 0
+             parse_mapping_value =  /#{conf[(CONF_MAPPING_HEADER + mapping_field).to_sym]} = /.match(line)
               
-              next if parse_mapping_value == nil
+            next if parse_mapping_value == nil
 #              custom_field_list.store(mapping_field.to_sym, parse_mapping_value.post_match.rstrip)
-              if mapping_field == 'trigger_value' then
-                custom_fields.store(conf[(CONF_CUSTOM_FIELD_ID_HEADER + mapping_field).to_sym].to_s,
-                                    get_trigger_value_to_conf(parse_mapping_value.post_match.rstrip, conf))
-              elsif mapping_field == 'trigger_nseverity' then
-                custom_fields.store(conf[(CONF_CUSTOM_FIELD_ID_HEADER + mapping_field).to_sym].to_s,
-                                    get_trigger_nseverity_to_conf(parse_mapping_value.post_match.rstrip, conf))
-              elsif mapping_field == 'date' then
-                date = parse_mapping_value.post_match.rstrip
-                custom_fields.store(conf[(CONF_CUSTOM_FIELD_ID_HEADER + mapping_field).to_sym].to_s, date.gsub(/./, '-'))
-              elsif mapping_field == 'event_date' then
-                event_date = parse_mapping_value.post_match.rstrip                event_date.gsub(/./, '-')
-                custom_fields.store(conf[(CONF_CUSTOM_FIELD_ID_HEADER + mapping_field).to_sym].to_s, event_date.gsub(/./, '-'))           
-              else
-                custom_fields.store(conf[(CONF_CUSTOM_FIELD_ID_HEADER + mapping_field).to_sym].to_s,parse_mapping_value.post_match.rstrip)
-              end
+            if mapping_field == 'trigger_value' then
+                  custom_fields.store(conf[(CONF_CUSTOM_FIELD_ID_HEADER + mapping_field).to_sym].to_s,
+                                      get_trigger_value_to_conf(parse_mapping_value.post_match.rstrip, conf))
+            elsif mapping_field == 'trigger_nseverity' then
+                  custom_fields.store(conf[(CONF_CUSTOM_FIELD_ID_HEADER + mapping_field).to_sym].to_s,
+                                      get_trigger_nseverity_to_conf(parse_mapping_value.post_match.rstrip, conf))
+            elsif mapping_field == 'date' then
+                  date = parse_mapping_value.post_match.rstrip
+                  custom_fields.store(conf[(CONF_CUSTOM_FIELD_ID_HEADER + mapping_field).to_sym].to_s, date.gsub('.', '-'))
+            elsif mapping_field == 'event_date' then
+                  event_date = parse_mapping_value.post_match.rstrip
+                  custom_fields.store(conf[(CONF_CUSTOM_FIELD_ID_HEADER + mapping_field).to_sym].to_s, event_date.gsub('.', '-'))           
+            else
+                  custom_fields.store(conf[(CONF_CUSTOM_FIELD_ID_HEADER + mapping_field).to_sym].to_s,parse_mapping_value.post_match.rstrip)
             end
           end
         end
+      end
+      # event id is registed
+      if custom_fields[conf[:custom_field_id_trigger_value].to_s].to_s == conf[:off_event].to_s
+        p 'update'
+        search_event_id = 'cf_' + conf[:custom_field_id_off_event_id].to_s
+      else
+        p 'post'
+        search_event_id = 'cf_' + conf[:custom_field_id_event_id].to_s
+      end
+      issues = RedmineClient::Issue.find(:all,
+                :params => {
+                   search_event_id.to_sym => custom_fields[conf[:custom_field_id_event_id].to_s].to_i
+                })
+      if issues.size > 0
+        puts "not covered EVENT.ID=" + custom_fields[conf[:custom_field_id_event_id].to_s]
+        return
+      end
 
-        mail_subject = MAIL_ENCODER.call(t_mail.subject.to_s)
-#        mail_body = MAIL_ENCODER.call(t_mail.body.to_s)
+      if !check_and_update(custom_fields, conf)
 
-        # event id is registed
-        if custom_fields[conf[:custom_field_id_trigger_value].to_s].to_s == conf[:off_event].to_s
-          p 'update'
-          search_event_id = 'cf_' + conf[:custom_field_id_off_event_id].to_s
-        else
-          p 'post'
-          search_event_id = 'cf_' + conf[:custom_field_id_event_id].to_s
-        end
-        issues = RedmineClient::Issue.find(:all,
-                  :params => {
-                     search_event_id.to_sym => custom_fields[conf[:custom_field_id_event_id].to_s]
-                  })
-        if issues.size > 0
-          puts "not covered EVENT.ID=" + custom_fields[conf[:custom_field_id_event_id].to_s]
-          next
-        end
-#        next if issues.size > 0
-
-        if !check_and_update(custom_fields, conf)
-
-          issue = RedmineClient::Issue.new(
-            :subject => mail_subject,
-            :description => mail_body,
-            :project_id => conf[:regist_project_id],
-            :custom_field_values => custom_fields
-          )
-          
-          begin
-            if issue.save
-              puts 'POST Issue ID=' +
-                   issue.id
-            else
-             puts issue.errors.full_messages
-            end
-          rescue
+        issue = RedmineClient::Issue.new(
+          :subject => mail_subject,
+          :description => mail_body,
+          :project_id => conf[:regist_project_id],
+          :custom_field_values => custom_fields
+        )
+        
+        begin
+          if issue.save
+            puts 'POST Issue ID=' +
+                 issue.id
+          else
+            p 'error'
+#             puts issue.errors.full_messages
+          end
+        rescue
 #            $hinemosTracLog.puts_message "Failure to create ticket to the trac server.Please Check trac server configuration."
 #            break
-          else
+        else
 #            $hinemosTracLog.puts_message "Success to create ticket ( id = #{issue.id} )"
-          end
         end
+      end
 #        if conf[:pop_mail_delete_enable] && mail_session.pop?
 #          mail_session.delete_pop_mail(i)
 #          $hinemosTracLog.puts_message "The mail was deleted in mail server."
@@ -206,13 +237,13 @@ module MailPicker
 #        end
 #      end
     end
+###############################################################################################
     
-    mail_session.finalize
 
 #   $hinemosTracLog.puts_message "Finished accessing the mail server."
 
 #    $hinemosTracLog.finalize
-  end
+
 
 #
 # update issue
@@ -225,17 +256,15 @@ module MailPicker
     issues = RedmineClient::Issue.find(:all,
               :params => {
                  search_hostname.to_sym => custom_fields[conf[:custom_field_id_hostname].to_s],
-                 search_trigger_id.to_sym => custom_fields[conf[:custom_field_id_trigger_id].to_s]
+                 search_trigger_id.to_sym => custom_fields[conf[:custom_field_id_trigger_id].to_s].to_i
               })
 
     issues.each do |issue|
       custom_field_trigger_value = issue.custom_fields.select{|elem| elem.name == conf[:mapping_trigger_value]}
-
       if custom_field_trigger_value[0].value != conf[:off_event].to_s
         set_custom_field(issue, conf[:mapping_off_event_id], custom_fields[conf[:custom_field_id_event_id].to_s])
         set_custom_field(issue, conf[:mapping_trigger_value], custom_fields[conf[:custom_field_id_trigger_value].to_s])
         set_custom_field(issue, conf[:mapping_trigger_name], custom_fields[conf[:custom_field_id_trigger_name].to_s])
-
         if issue.save
           puts 'UPDATE Issue ID=' +
                issue.id
@@ -296,34 +325,7 @@ module MailPicker
     return str
   end
 
-  def get_event_from_zabbix(conf)
-    p 'test3'
-#    zbx = Zabbix::ZabbixApi.new(conf[:zabbix_url], conf[:zabbix_user_id], conf[:zabbix_user_password])
-zbx = Zabbix::ZabbixApi.new('http://172.17.1.207/zabbix/api_jsonrpc.php', 'admin','zabbix')
-
-    p zbx
-#    hostid = zbx.get_host_id('s-ibs-portal-stg01')
-    h = zbx.get_host_id('s-ibs-portal-stg01')
-
-    p hostid
-    message_get_host = { 
-       :method => 'host.get', 
-       :params => { 
-       :triggerids =>[ev["objectid"]], 
-       :output => 'extend' 
-      }#, 
-      #:auth => zbx.auth 
-    } 
-    p 'message_get_host'
-    host = zbx.do_request(message_get_host)  
-
-
-
-    p host 
-
-
-  end
-  module_function :main, :check_and_update, :set_custom_field, :target_mail?, :get_trigger_value_to_conf, :get_trigger_nseverity_to_conf, :get_event_from_zabbix
+  module_function :main, :check_and_update, :set_custom_field, :target_mail?, :get_trigger_value_to_conf, :get_trigger_nseverity_to_conf, :regist_issue
 end
 
 MailPicker.main
